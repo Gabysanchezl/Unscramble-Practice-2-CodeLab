46b904717e29bba43a4ce96ba0eba068acdc4ce9
gabysanchezl <lgab.sanchez@gmail.com>
2025-09-21 17:08:58 -0400

    "practica 2"

--- app/src/main/java/com/example/unscramble/ui/theme/Color.kt
+++ app/src/main/java/com/example/unscramble/ui/theme/Color.kt
@@ -18,6 +18,7 @@ package com.example.unscramble.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
 val md_theme_light_primary = Color(0xFF009688)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_primaryContainer = Color(0xFFDEE0FF)
